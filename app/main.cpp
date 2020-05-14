064c090add12143b11cf69760cdec78f8c8f9a09
rauljgarcia <rauljgarcia97@gmail.com>
2020-05-13 22:54:05 -0600

    switch stmt on menu

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include "../code/RateCalc.h"
+#define UNDERLINE "\033[4m"
+#define CLOSEUNDERLINE "\033[0m"
 
 using namespace std;
 
 int main(){
 
+int choice;
 
+do {
+    for (int i=0; i<20; i++){
+        cout<<"\n";
+    }
+    cout<<UNDERLINE<<"Co-Funder Cost Sharing Calculator"<<CLOSEUNDERLINE<<endl;
+    cout<<"\n";
+    cout<<"0. To quit."<<endl;
+    cout<<"1. To start."<<endl;
 
+    cin>>choice;
+
+    switch (choice)
+    {
+    case 0:
+        cout<<"Ending."<<endl;
+        return  0;
+
+    case 1:
+        cout<<"let's go"<<endl;
+        break;
+    }
+}
+while(choice != 0);
 }
